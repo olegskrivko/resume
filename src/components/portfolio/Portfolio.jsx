6ad7f4ff5d7125941dbf8939475d582170acb0ec
Olegs <olegs.krivko@gmail.com>
2023-06-29 21:51:05 +0300

    remove old repo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,22 +12,10 @@ import shoppingcart from "../../assets/shoppingcart.jpg";
 function Items() {
   const portfolioItems = [
     {
-      name: "Expense Tracker",
-      imgSrc: dashboard,
-      gitUrl: "https://github.com/olegskrivko/expenses",
-      demoUrl: "https://olegskrivko.github.io/expenses/",
-    },
-    {
       name: "Weather App",
       imgSrc: weatherapp,
       gitUrl: "https://github.com/olegskrivko/weather",
       demoUrl: "https://olegskrivko.github.io/weather/",
-    },
-    {
-      name: "Calculator",
-      imgSrc: calculator,
-      gitUrl: "https://github.com/olegskrivko/calculator",
-      demoUrl: "https://olegskrivko.github.io/calculator/",
     },
     {
       name: "To-Do",
@@ -42,10 +30,10 @@ function Items() {
       demoUrl: "https://gentle-pink-hospital-gown.cyclic.app/api/recipes",
     },
     {
-      name: "Animal Search",
+      name: "PawPrint",
       imgSrc: animals,
-      gitUrl: "https://github.com/olegskrivko/animalsearch",
-      demoUrl: "https://weak-teal-piglet-kit.cyclic.app/pets",
+      gitUrl: "https://github.com/olegskrivko/pawprint",
+      demoUrl: "https://pawprint.cyclic.app",
     },
     {
       name: "Resume",
